6aed76b07b03028d84dc8bb5565cea63c7054bb1
Mac Mbizo <macmbizo@gmail.com>
2021-11-29 21:47:32 +0200

    added a test image

--- basics.py
+++ basics.py
@@ -2,9 +2,20 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgElon = face_recognition.load_image_file('ImageBasic/Elon Musk.jpg')
+imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImageBasic/Elon Test.jpg')
+imgTest = face_recognition.load_image_file('ImagesBasic/Elon Test.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Elon Test', imgTest)
+cv2.waitKey(0)
+
+
+
+
+
+
+
 
 
