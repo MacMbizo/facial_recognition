2f509e8237528eb95ef6a54a8498aeca54dd6527
Mac Mbizo <macmbizo@gmail.com>
2022-03-20 01:47:16 +0200

    created lists and code for reading pictures in a folder.

--- AccessControlProject.py
+++ AccessControlProject.py
@@ -1,8 +1,18 @@
+import os
+
 import cv2
 import face_recognition
 
 path = 'ImagesAccess'
 images = []
+classNames = []
+mylist = os.listdir(path)
+print(mylist)  # checking contents
+for cl in mylist:
+    curImage = cv2.imread(f'{path}/{cl}')
+    images.append(curImage)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)  # checking contents after split
 
 imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
